b4838f8201347c099db9aaa2a04975cbd34b3b3a
pratikshaChaturvedi <60311430+pratikshaChaturvedi@users.noreply.github.com>
2021-02-19 20:22:20 +0530

    Initial Server Commit

---
+++ index.js
@@ -0,0 +1,34 @@
+let express = require('express');
+let bodyParser = require('body-parser');
+let mongoose = require('mongoose');
+require('dotenv').config();
+let app = express();
+let apiRoutes = require("./api-routes");
+app.use(bodyParser.urlencoded({
+    extended : true
+}));
+app.use(bodyParser.json());
+mongoose.connect(
+    process.env.uri,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    },
+    (err) => {
+      if (err) throw err;
+      console.log("MongoDB connection established");
+    }
+  );
+// mongoose.connect('mongodb://localhost/myDb', { useNewUrlParser : true, useUnifiedTopology: true});
+var db = mongoose.connection;
+if(!db)
+    console.log("Error connecting to db");
+else
+    console.log("DB connected successfully");
+var port = process.env.PORT || 8080;
+app.get('/', (req,res) => res.send('Express server up with nodemon'));
+app.use('/api', apiRoutes);
+app.listen(port, function() {
+    console.log("Running restcrud on port " +port);
+});
